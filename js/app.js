f7a9aaa1fe4df464f735656aef43bd9218fb5431
salehmmasri <salehalmasri494@yahoo.com>
2020-06-02 20:09:51 +0300

    update code without include

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@
 'use strict';
 //array with Pic Name
 // console.log('welcome');
+var itemArr = [];
 var prodact = [
     "bag.jpg",
     "banana.jpg",
@@ -15,116 +16,139 @@ var prodact = [
     "chair.jpg",
     "cthulhu.jpg",
     "scissors.jpg",
-    "wine-glass.jpg",
-    "usb.gif",
-    "shark.jpg",
-    "dragon.jpg",
-    "sweep.png",
-    "tauntaun.jpg",
-    "unicorn.jpg",
-    "water-can.jpg",
-    "pen.jpg",
-    "dog-duck.jpg",
+    // "wine-glass.jpg",
+    // "usb.gif",
+    // "shark.jpg",
+    // "dragon.jpg",
+    // "sweep.png",
+    // "tauntaun.jpg",
+    // "unicorn.jpg",
+    // "water-can.jpg",
+    // "pen.jpg",
+    // "dog-duck.jpg",
     "pet-sweep.jpg"
 
 ];
+
+var leftItem, centerItem, rightItem;
+function SaveTheData() {
+    var prodactArray = JSON.stringify(itemArr);
+    localStorage.setItem('prodactInLocal', prodactArray);
+}
+function getSavedData() {
+    var getProdactArray = localStorage.getItem('prodactInLocal');
+    if (getProdactArray) {
+        //console.log('vvv');
+        //console.log(getProdactArray);
+        itemArr = JSON.parse(getProdactArray);
+        // renderImages();
+        //console.log(itemArr.views);
+
+
+    }
+
+}
 var leftImg = document.getElementById("leftImage");
 var centerImg = document.getElementById("centerImage");
 var rightImg = document.getElementById("rightImage");
 var imagesSection = document.getElementById('imagesSection');
 var totalClickNumber = 0;
-var x=0;
-var arr=[];
+var x = 0;
 //Constractor
 function Items(name) {
     this.itemName = name;
     this.imagePath = `assets/${name}`;
     this.clickNumber = 0;
-    this.views=0;
-    Items.all.push(this);
-    arr.push(this);
+    this.views = 0;
+    itemArr.push(this);
+
+    // eslint-disable-next-line new-cap
 }
-//array to contain all objects
-
-Items.all = [];
 // create objects based on the Prodact array
-
 for (var i = 0; i < prodact.length; i++) {
     new Items(prodact[i]);
 
 }
 
-//  console.log(Items.all);
+var NumberEvenOrod = 0;
+var arrayOfItems2 = [];
+var arrayOfItems = [];
 
-//var y=0;
-var arrayOfItems=[];
-var leftItem, centerItem, rightItem;
-//var arrayOfItems=[];
 //display random images
 
-
+var iterationNumber = 0;
 function renderImages() {
-    leftItem = Items.all[randomNumber(0, Items.all.length - 1)];
-    centerItem = Items.all[randomNumber(0, Items.all.length - 1)];
-    rightItem = Items.all[randomNumber(0, Items.all.length - 1)];
+    leftItem = itemArr[randomNumber(0, itemArr.length - 1)];
+    centerItem = itemArr[randomNumber(0, itemArr.length - 1)];
+    rightItem = itemArr[randomNumber(0, itemArr.length - 1)];
     //check if we have the same pic
-
-    while (leftItem === centerItem || leftItem === rightItem||rightItem === centerItem
-        || arrayOfItems.includes(leftItem) ||arrayOfItems.includes(centerItem)||arrayOfItems.includes(rightItem)
-        ) {
-    rightItem = Items.all[randomNumber(0, Items.all.length - 1)];
-    centerItem = Items.all[randomNumber(0, Items.all.length - 1)];
-    leftItem = Items.all[randomNumber(0, Items.all.length - 1)];
-    arrayOfItems=[];
-}
-arrayOfItems.push(leftItem);
-arrayOfItems.push(centerItem);
-arrayOfItems.push(rightItem);
-
-
-    //     if(y%2===0){
-    //     arrayOfItems=[];
-    //     arrayOfItems.push(leftItem,centerItem,rightItem);
-    //     console.log("y now is "+ y);
-
-
-    //     console.log(arrayOfItems);
+    //************************************ this one for only 3 whitout sequantal/
+    // while (leftItem === centerItem || leftItem === rightItem || rightItem === centerItem) {
+    //     rightItem = itemArr[randomNumber(0, itemArr.length - 1)];
+    //     centerItem = itemArr[randomNumber(0, itemArr.length - 1)];
+    //     leftItem = itemArr[randomNumber(0, itemArr.length - 1)];
     // }
-    //     else if(y%2===1) {
 
-    //         console.log(leftItem.imagePath);
+    if (NumberEvenOrod % 2 === 0) {
+        arrayOfItems = [];
+        arrayOfItems.push(leftItem);
+        arrayOfItems.push(centerItem);
+        arrayOfItems.push(rightItem);
+        if (iterationNumber === 0) {
+            console.log("p");
+        }
+        else {
+            for (var i = 0; i < arrayOfItems2.length; i++) {
+                for (var j = 0; j < arrayOfItems.length; j++) {
+                    if (arrayOfItems2[i] === arrayOfItems[j] || leftItem === centerItem || leftItem === rightItem || rightItem === centerItem) {
+                        arrayOfItems = [];
+                        i = 0;
+                        j = 0;
+                        rightItem = itemArr[randomNumber(0, itemArr.length - 1)];
+                        centerItem = itemArr[randomNumber(0, itemArr.length - 1)];
+                        leftItem = itemArr[randomNumber(0, itemArr.length - 1)];
+                        arrayOfItems.push(leftItem);
+                        arrayOfItems.push(centerItem);
+                        arrayOfItems.push(rightItem);
 
-    //         while (arrayOfItems[0].imagePath==leftItem.imagePath||arrayOfItems[1].imagePath==leftItem.imagePath||arrayOfItems[1].imagePath==leftItem.imagePath) {
-    //             console.log("left");
-    //             renderImages();
-    //             console.log(leftItem);
-    //         }
+                    }
+                }
 
+            }
+        }
+        console.log(`the items in array one ${iterationNumber}`, arrayOfItems);
 
-            // var arrayOfItems2=[];
-            // arrayOfItems2.push(leftItem,centerItem,rightItem);
+    }
+    else {
+        arrayOfItems2 = [];
+        arrayOfItems2.push(leftItem);
+        arrayOfItems2.push(centerItem);
+        arrayOfItems2.push(rightItem);
+        for (let i = 0; i < arrayOfItems.length; i++) {
+            for (let j = 0; j < arrayOfItems2.length; j++) {
+                if (arrayOfItems[i] === arrayOfItems2[j] || leftItem === centerItem || leftItem === rightItem || rightItem === centerItem) {
+                    arrayOfItems2 = [];
+                    i = 0;
+                    j = 0;
+                    rightItem = itemArr[randomNumber(0, itemArr.length - 1)];
+                    centerItem = itemArr[randomNumber(0, itemArr.length - 1)];
+                    leftItem = itemArr[randomNumber(0, itemArr.length - 1)];
+                    arrayOfItems2.push(leftItem);
+                    arrayOfItems2.push(centerItem);
+                    arrayOfItems2.push(rightItem);
 
-            // console.log(y);
+                }
+            }
 
-            //     for (var i = 0; i < arrayOfItems2.length; i++) {
-            //     while(arrayOfItems2[i]===arrayOfItems[0])
+        }
+        console.log(`the items in ${iterationNumber}`, arrayOfItems2);
 
-            //     }
-            //     while(arrayOfItems2[0]===arrayOfItems[0]||arrayOfItems2[1]===arrayOfItems[1]||arrayOfItems2[2]===arrayOfItems[2]){
-            //         renderImages();
-            //         arrayOfItems2.length=0;
-            //         arrayOfItems2.push(leftItem,centerItem,rightItem);
-            //         console.log(arrayOfItems2);
+    }
+    NumberEvenOrod++;
 
-            //     }
+    //        console.log('iteration ', iterationNumber);
+    iterationNumber++;
 
-
-    //}
-
-
-
-
-    //left Imge
 
     leftImg.src = leftItem.imagePath;
     leftImg.alt = leftItem.itemName;
@@ -144,6 +168,9 @@ arrayOfItems.push(rightItem);
     rightImg.title = rightItem.itemName;
     rightItem.views++;
 
+    // eslint-disable-next-line new-cap
+    //SaveTheData();
+
 }
 //Invoke the function to display img
 renderImages();
@@ -151,49 +178,50 @@ renderImages();
 imagesSection = addEventListener('click', actionClick);
 
 function actionClick(event) {
-    if (totalClickNumber<25) {
-    if (event.target.id === 'leftImage' || event.target.id === 'centerImage' || event.target.id === 'rightImage') {
-        totalClickNumber++;
+    if (totalClickNumber < 5) {
+        if (event.target.id === 'leftImage' || event.target.id === 'centerImage' || event.target.id === 'rightImage') {
+            totalClickNumber++;
 
-        if (event.target.id === 'leftImage') {
-            //console.log("left");
-            leftItem.clickNumber++;
+            if (event.target.id === 'leftImage') {
+                //console.log("left");
+                leftItem.clickNumber++;
 
+            }
+            if (event.target.id === 'centerImage') {
+                //console.log("mid");
+                centerItem.clickNumber++;
+
+            }
+            if (event.target.id === 'rightImage') {
+                rightItem.clickNumber++;
+
+            }
+
+
+            //Invoke the  renderImages function to show new pic
+            renderImages();
+            //arrayOfItems=[];
         }
-        if (event.target.id === 'centerImage') {
-            //console.log("mid");
-            centerItem.clickNumber++;
-
-        }
-        if (event.target.id === 'rightImage') {
-            rightItem.clickNumber++;
-
-        }
-
-
-        //Invoke the  renderImages function to show new pic
-        renderImages();
     }
-}
-else{
-    if(x===0){
-        x++;
-    printResultofClicksandViews();
-    viewChart();
+    else {
+        if (x === 0) {
+            x++;
+            printResultofClicksandViews();
+            viewChart();
 
+        }
     }
-}
 
 }
 
-function printResultofClicksandViews () {
+function printResultofClicksandViews() {
     var ulE1 = document.getElementById('finalResult');
-    for( var i =0; i<Items.all.length; i++) {
-      var li = document.createElement('li');
-      li.textContent = `${Items.all[i].itemName.split(".",1)} has ${Items.all[i].clickNumber} clicks and ${Items.all[i].views} views`;
-      ulE1.append(li);
+    for (var i = 0; i < itemArr.length; i++) {
+        var li = document.createElement('li');
+        li.textContent = `${itemArr[i].itemName.split(".", 1)} has ${itemArr[i].clickNumber} clicks and ${itemArr[i].views} views`;
+        ulE1.append(li);
     }
-  }
+}
 
 
 
@@ -215,105 +243,107 @@ function randomNumber(min, max) {
 
 
 function viewChart() {
-   
-var prodactName=[];
-var viewsOnChart=[];
-var clickOnChart=[];
-for(var i =0;i<arr.length;i++){
-    var storeName=arr[i].itemName.split(".",1);
-    prodactName.push(storeName);
 
-    var storeViews= arr[i].views;
-    viewsOnChart.push(storeViews);
+    var prodactName = [];
+    var viewsOnChart = [];
+    var clickOnChart = [];
+    for (var i = 0; i < itemArr.length; i++) {
+        var storeName = itemArr[i].itemName.split(".", 1);
+        prodactName.push(storeName);
 
-    var storeClick=arr[i].clickNumber;
-    clickOnChart.push(storeClick);
- }
-  
+        var storeViews = itemArr[i].views;
+        viewsOnChart.push(storeViews);
+
+        var storeClick = itemArr[i].clickNumber;
+        clickOnChart.push(storeClick);
+    }
+
     // chart js code
     var ctx = document.getElementById('myChart').getContext('2d');
+    // eslint-disable-next-line no-undef
     var myChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: prodactName,
-        datasets: [
-          {
-            label: '# of clicks',
-            data: clickOnChart,
-            backgroundColor: [
-              'rgba(135, 75, 108,1)',
-              'rgba(135, 75, 108,1)',
-              'rgba(135, 75, 108,1)',
-              'rgba(135, 75, 108,1)',
-              'rgba(135, 75, 108,1)',
-              'rgba(135, 75, 108,1)',
-              'rgba(135, 75, 108,1)',
-              'rgba(135, 75, 108,1)',
-              'rgba(135, 75, 108,1)',
-              'rgba(135, 75, 108,1)',
-              'rgba(135, 75, 108,1)',
-              'rgba(135, 75, 108,1)',
-              'rgba(135, 75, 108,1)',
-              'rgba(135, 75, 108,1)',
-              'rgba(135, 75, 108,1)',
-              'rgba(135, 75, 108,1)',
-              'rgba(135, 75, 108,1)',
-              'rgba(135, 75, 108,1)',
-              'rgba(135, 75, 108,1)',
-              'rgba(135, 75, 108,1)',
-              'rgba(135, 75, 108,1)',
-              'rgba(135, 75, 108,1)',
-              'rgba(135, 75, 108,1)',
-              'rgba(135, 75, 108,1)',
-              'rgba(135, 75, 108,1)'
-            ],
-            borderWidth: 1
-          },
-          {
-            label: '# of views',
-            data: viewsOnChart,
-            backgroundColor: [
-              'rgba(255, 87, 115,1)',
-              'rgba(255, 87, 115,1)',
-              'rgba(255, 87, 115,1)',
-              'rgba(255, 87, 115,1)',
-              'rgba(255, 87, 115,1)',
-              'rgba(255, 87, 115,1)',
-              'rgba(255, 87, 115,1)',
-              'rgba(255, 87, 115,1)',
-              'rgba(255, 87, 115,1)',
-              'rgba(255, 87, 115,1)',
-              'rgba(255, 87, 115,1)',
-              'rgba(255, 87, 115,1)',
-              'rgba(255, 87, 115,1)',
-              'rgba(255, 87, 115,1)',
-              'rgba(255, 87, 115,1)',
-              'rgba(255, 87, 115,1)',
-              'rgba(255, 87, 115,1)',
-              'rgba(255, 87, 115,1)',
-              'rgba(255, 87, 115,1)',
-              'rgba(255, 87, 115,1)',
-              'rgba(255, 87, 115,1)',
-              'rgba(255, 87, 115,1)',
-              'rgba(255, 87, 115,1)',
-              'rgba(255, 87, 115,1)',
-              'rgba(255, 87, 115,1)'
-            ],
-  
-            borderWidth: 1
-          }
-        ]
-      },
-      options: {
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true
-              }
+        type: 'bar',
+        data: {
+            labels: prodactName,
+            datasets: [
+                {
+                    label: '# of clicks',
+                    data: clickOnChart,
+                    backgroundColor: [
+                        'rgba(135, 75, 108,1)',
+                        'rgba(135, 75, 108,1)',
+                        'rgba(135, 75, 108,1)',
+                        'rgba(135, 75, 108,1)',
+                        'rgba(135, 75, 108,1)',
+                        'rgba(135, 75, 108,1)',
+                        'rgba(135, 75, 108,1)',
+                        'rgba(135, 75, 108,1)',
+                        'rgba(135, 75, 108,1)',
+                        'rgba(135, 75, 108,1)',
+                        'rgba(135, 75, 108,1)',
+                        'rgba(135, 75, 108,1)',
+                        'rgba(135, 75, 108,1)',
+                        'rgba(135, 75, 108,1)',
+                        'rgba(135, 75, 108,1)',
+                        'rgba(135, 75, 108,1)',
+                        'rgba(135, 75, 108,1)',
+                        'rgba(135, 75, 108,1)',
+                        'rgba(135, 75, 108,1)',
+                        'rgba(135, 75, 108,1)',
+                        'rgba(135, 75, 108,1)',
+                        'rgba(135, 75, 108,1)',
+                        'rgba(135, 75, 108,1)',
+                        'rgba(135, 75, 108,1)',
+                        'rgba(135, 75, 108,1)'
+                    ],
+                    borderWidth: 1
+                },
+                {
+                    label: '# of views',
+                    data: viewsOnChart,
+                    backgroundColor: [
+                        'rgba(255, 87, 115,1)',
+                        'rgba(255, 87, 115,1)',
+                        'rgba(255, 87, 115,1)',
+                        'rgba(255, 87, 115,1)',
+                        'rgba(255, 87, 115,1)',
+                        'rgba(255, 87, 115,1)',
+                        'rgba(255, 87, 115,1)',
+                        'rgba(255, 87, 115,1)',
+                        'rgba(255, 87, 115,1)',
+                        'rgba(255, 87, 115,1)',
+                        'rgba(255, 87, 115,1)',
+                        'rgba(255, 87, 115,1)',
+                        'rgba(255, 87, 115,1)',
+                        'rgba(255, 87, 115,1)',
+                        'rgba(255, 87, 115,1)',
+                        'rgba(255, 87, 115,1)',
+                        'rgba(255, 87, 115,1)',
+                        'rgba(255, 87, 115,1)',
+                        'rgba(255, 87, 115,1)',
+                        'rgba(255, 87, 115,1)',
+                        'rgba(255, 87, 115,1)',
+                        'rgba(255, 87, 115,1)',
+                        'rgba(255, 87, 115,1)',
+                        'rgba(255, 87, 115,1)',
+                        'rgba(255, 87, 115,1)'
+                    ],
+
+                    borderWidth: 1
+                }
+            ]
+        },
+        options: {
+            scales: {
+                yAxes: [
+                    {
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }
+                ]
             }
-          ]
         }
-      }
     });
-  }
+}
+//getSavedData();
